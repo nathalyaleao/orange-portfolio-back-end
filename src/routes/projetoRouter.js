5355b87c79d1278f67dde6054a08ed83c275a7ef
Bruno <bruno.silva.castilho@gmail.com>
2024-01-28 14:00:19 -0300

    Adicionado middleware para controle de sessão

--- src/routes/projetoRouter.js
+++ src/routes/projetoRouter.js
@@ -1,15 +1,15 @@
 const { Router } = require('express');
 const ProjetoController = require('../controllers/ProjetoController.js');
-
+const session = require('../middlewares/session.js')
 const projetoController = new ProjetoController();
 const router = Router();
 
-router.get('/projetos', (req, res) => projetoController.pegaTodos(req, res)); //Pega todos
-router.get('/projetos/:id', (req, res) => projetoController.pegaUmPorId(req, res));
-router.get('/projetos/user/:id', (req, res) => projetoController.pegaTodosdeUmUsuario(req, res)); // Pega de usuario
-router.post('/projetos', (req, res) => projetoController.criaNovo(req, res));
-router.put('/projetos/:id', (req, res) => projetoController.atualiza(req, res));
-router.delete('/projetos/:id', (req, res) => projetoController.exclui(req, res));
+router.get('/projetos', session.onlyUsers, (req, res) => projetoController.pegaTodos(req, res)); //Pega todos
+router.get('/projetos/user', session.onlyUsers, (req, res) => projetoController.pegaTodosdeUmUsuario(req, res)); // Pega de usuario
+router.get('/projetos/:id', session.onlyUsers, (req, res) => projetoController.pegaUmPorId(req, res));
+router.post('/projetos', session.onlyUsers, (req, res) => projetoController.criaNovo(req, res));
+router.put('/projetos/:id', session.onlyUsers, (req, res) => projetoController.atualiza(req, res));
+router.delete('/projetos/:id', session.onlyUsers, (req, res) => projetoController.exclui(req, res));
 
 module.exports = router;
 
