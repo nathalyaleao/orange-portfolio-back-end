b6b582b6ddc4cece7c76d45c48e669d896460446
Bruno <bruno.silva.castilho@gmail.com>
2024-01-28 15:56:49 -0300

    fixed bug mysql2 vercel

--- src/db.js
+++ src/db.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require("sequelize") // importar o sequelize
+const mysql = require("mysql2")
 
 
 const DB_NAME = process.env.DB_NAME; 
@@ -11,6 +12,7 @@ const DB_PORT = process.env.DB_PORT
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   //passar os dados para o sequelize
   dialect: "mysql", //informar o tipo de banco que vamos utilizar
+  dialectModule: mysql2,
   host: DB_HOST, //o host, neste caso estamos com um banco local
   port: DB_PORT,
 });
