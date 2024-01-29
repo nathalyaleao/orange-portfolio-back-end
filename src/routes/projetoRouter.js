c97f6c96d735d4a874df7ead097f81a57c50afb1
Nathalya <nathalyamarques2021@gmail.com>
2024-01-29 17:51:52 -0300

    recebendo upload da imagem no cadastro do portfolio

--- src/routes/projetoRouter.js
+++ src/routes/projetoRouter.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const ProjetoController = require('../controllers/ProjetoController.js');
-const session = require('../middlewares/session.js')
+const session = require('../middlewares/session.js');
+const uploadImage = require("../middlewares/uploadImage.js")
 const projetoController = new ProjetoController();
 const router = Router();
 
 router.get('/projetos', session.onlyUsers, (req, res) => projetoController.pegaTodos(req, res)); //Pega todos
 router.get('/projetos/user', session.onlyUsers, (req, res) => projetoController.pegaTodosdeUmUsuario(req, res)); // Pega de usuario
 router.get('/projetos/:id', session.onlyUsers, (req, res) => projetoController.pegaUmPorId(req, res));
-router.post('/projetos', session.onlyUsers, (req, res) => projetoController.criaNovo(req, res));
+router.post('/projetos', session.onlyUsers, uploadImage.single("arquivo"), (req, res) => projetoController.criaNovo(req, res));
 router.put('/projetos/:id', session.onlyUsers, (req, res) => projetoController.atualiza(req, res));
 router.delete('/projetos/:id', session.onlyUsers, (req, res) => projetoController.exclui(req, res));
 
