04bb27f41e46a123e11217e7722a99a6bf2c5850
Bruno <bruno.silva.castilho@gmail.com>
2024-01-30 14:56:41 -0300

    Removidas rotas desnecessárias

--- src/routes/usuariosRouter.js
+++ src/routes/usuariosRouter.js
@@ -4,11 +4,11 @@ const UsuarioController = require('../controllers/UsuarioController.js');
 const usuarioController = new UsuarioController();
 const router = Router();
 
-router.get('/usuarios', (req, res) => usuarioController.pegaTodos(req, res));
-router.get('/usuarios/:id', (req, res) => usuarioController.pegaUmPorId(req, res));
-router.post('/usuarios', (req, res) => usuarioController.criaNovo(req, res)); // Cria usuario
-router.patch('/usuarios/:id', (req, res) => usuarioController.atualiza(req, res));
-router.delete('/usuarios/:id', (req, res) => usuarioController.exclui(req, res));
+// router.get('/usuarios', (req, res) => usuarioController.pegaTodos(req, res));
+// router.get('/usuarios/:id', (req, res) => usuarioController.pegaUmPorId(req, res));
+router.post('/usuarios', (req, res) => usuarioController.criaNovo(req, res));
+// router.patch('/usuarios/:id', (req, res) => usuarioController.atualiza(req, res));
+// router.delete('/usuarios/:id', (req, res) => usuarioController.exclui(req, res));
 
 module.exports = router;
 
