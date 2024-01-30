dda30a66a28e590046bf69d961bb032cb788ac09
Jessica Souza <jsdl.9991@gmail.com>
2024-01-30 16:50:27 -0300

    adicionado get undefind  no campo senha

--- src/services/Services.js
+++ src/services/Services.js
@@ -16,7 +16,8 @@ class Services {
         return dataSource[this.model].findAll({
             where: {
                 user_id: id,
-            }});
+            }
+        });
     }
 
     async criaRegistro(dadosDoRegistro) {
