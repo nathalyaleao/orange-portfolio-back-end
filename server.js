1ecdc14bc5c41a3f1cb989d95d9baff1022e9fc1
Jessica Souza <jsdl.9991@gmail.com>
2024-01-29 21:00:07 -0300

    adicionado middleware para lidar com rotas não encontradas

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const createError = require('http-errors');
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
 const routes = require("./src/routes/index.js")
-
 
 const corsOptions = {
   origin: process.env.CLIENT_URL,
@@ -13,7 +13,6 @@ const corsOptions = {
   credentials: true,
 };
 
-
 const app = express();
 
 app.use(express.json());
@@ -21,6 +20,11 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors(corsOptions));
 routes(app);
-
-app.use(routes)
+app.use((req, res, next) => {
+  next(createError(404));
+})
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err);
+});
 app.listen(3000, () => console.log(`Servidor iniciado na porta 3000`));
