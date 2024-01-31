413bd08b7bb18d2ae33c634b5480d4dbb9ab42ce
Bruno <bruno.silva.castilho@gmail.com>
2024-01-31 13:07:53 -0300

    Busca de projetos com o nome do usuario

--- src/services/ProjetoServices.js
+++ src/services/ProjetoServices.js
@@ -1,7 +1,24 @@
 const Services = require('./Services.js')
+const dataSource = require('../models');
 class ProjetoServices extends Services {
     constructor() {
         super('Projeto');
     }
+    
+    async pegaRegistrosPorUsuario(id) {
+        return dataSource[this.model].findAll({
+            raw: true,
+            include: [{
+              model: dataSource['Usuario'],
+              required: true,
+              attributes: ['nome', 'sobrenome']
+            }],
+            where: {
+              user_id: id,
+            },
+            order: [['updatedAt', 'DESC' ]]
+          });
+    }
+
 }
 module.exports = ProjetoServices;
