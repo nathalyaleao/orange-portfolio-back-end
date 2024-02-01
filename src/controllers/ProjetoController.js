cb709f8dfaf99b7a1b5fb4b819b764da33cc5fa4
Bruno <bruno.silva.castilho@gmail.com>
2024-02-01 15:12:49 -0300

    adicionado id do usario na coluna user_id do projeto

--- src/controllers/ProjetoController.js
+++ src/controllers/ProjetoController.js
@@ -6,6 +6,22 @@ class ProjetoController extends Controller {
     constructor() {
         super(projetoServices);
     }
+
+    async criaNovo(req, res) {
+        //id
+        let dadosParaCriacao = req.body;
+
+        dadosParaCriacao = {...dadosParaCriacao, user_id: req.user.id}
+        
+        try {
+            const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
+            return res.status(200).json(novoRegistroCriado);
+        } catch (erro) {
+            console.log(erro.message)
+            
+            return res.status(400).json({ erro: erro.message })
+        }
+    }
 }
 
 module.exports = ProjetoController
